8568c5ddc3746967dca6e8d7cf9899df94ef4d27
Ben Wolthuis <ben.wolthuis@gmail.com>
2018-01-31 12:19:59 +0100

    pasted comments from UI to GameUI

--- src/main/java/nl/quintor/solitaire/ui/GameUI.java
+++ src/main/java/nl/quintor/solitaire/ui/GameUI.java
@@ -15,21 +15,46 @@ import nl.quintor.solitaire.models.state.GameState;
  */
 public class GameUI implements UI {
 
+    /**
+     * Set a message to show the player the next time {@link #refresh(GameState)} is called. This message
+     * should be shown as a non-error message.
+     *
+     * @param message message to show the player
+     */
     @Override
     public void setMessage(String message) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Set an error message to show the player the next time {@link #refresh(GameState)} is called. This
+     * message should be shown as an error.
+     *
+     * @param message error message to show the player
+     */
     @Override
     public void setErrorMessage(String message) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Signals to the UI implementation that the game state has changed. The UI may or may not refresh more often.
+     *
+     * @param gameState the game state to be visualized by the UI
+     */
     @Override
     public void refresh(GameState gameState) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Signals to the UI implementation that the game state has changed, and that player input is expected. The UI may
+     * or may not refresh more often.
+     *
+     * @param gameState the game state to be visualized by the UI
+     * @param moves the moves that are possible in this game state
+     * @return String representation of the player request
+     */
     @Override
     public String refreshAndRequestMove(GameState gameState, Collection<Move> moves) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
